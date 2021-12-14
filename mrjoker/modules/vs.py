a98a5c7b7a081a75a5c689c59752296049b00497
Vickyftw <90129845+Vickyftw@users.noreply.github.com>
2021-12-14 09:23:21 +0530

    Update vs.py

--- mrjoker/modules/vs.py
+++ mrjoker/modules/vs.py
@@ -16,14 +16,14 @@ __help__ = """
 *HOW TO USE THIS BOT:*
 1.) first, add me to your group.
 2.) then promote me as admin and give all permissions except anonymous admin.
-3.) add @Mrjokermusicbo to your group.
+3.) add @AngelxRobot to your group.
 4.) turn on the voice chat first before start to stream video.
 5.) type /vplay (reply to video) to start streaming.
 6.) type /vstop to end the video streaming.
 
 💡 *Note: stream & stop command can only be executed by group admin only!*
 
-Powered By : [video-stream](https://github.com/levina-lab/video-stream)
+Powered By : [video-stream](https://t.me/angelsupports)
 """
 
-__mod_name__ = "Video Stream"
+__mod_name__ = "VC Music"
