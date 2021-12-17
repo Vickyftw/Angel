6b2aad06f166fe2542625b2564d62959698e4498
Vickyftw <90129845+Vickyftw@users.noreply.github.com>
2021-12-17 10:32:45 +0530

    Update antiflood.py

--- mrjoker/modules/antiflood.py
+++ mrjoker/modules/antiflood.py
@@ -424,7 +424,7 @@ __help__ = """
  `1w` = 1 week
  """
 
-__mod_name__ = "Antiflood"
+__mod_name__ = "ᴀɴᴛɪ-ꜰʟᴏᴏᴅ"
 
 FLOOD_BAN_HANDLER = MessageHandler(
     Filters.all & ~Filters.status_update & Filters.group, check_flood
