e451daececae0eeb566d107e3f23213afa24d88a
Vickyftw <90129845+Vickyftw@users.noreply.github.com>
2022-01-05 21:32:11 +0530

    Update blacklist_stickers.py

--- mrjoker/modules/blacklist_stickers.py
+++ mrjoker/modules/blacklist_stickers.py
@@ -514,7 +514,7 @@ Note:
  🔹 `<sticker link>` can be `https://t.me/addstickers/<sticker>` or just `<sticker>` or reply to the sticker message
 """
 
-__mod_name__ = "Stickers Blacklist"
+__mod_name__ = "ꜱᴛɪᴄᴋᴇʀꜱ ʙʟᴀᴄᴋʟɪꜱᴛ⬛️"
 
 BLACKLIST_STICKER_HANDLER = DisableAbleCommandHandler(
     "blsticker", blackliststicker, admin_ok=True
