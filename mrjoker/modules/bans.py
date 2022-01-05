7e51bef6e6a472bf57151c9f114414be13aca8f0
Vickyftw <90129845+Vickyftw@users.noreply.github.com>
2022-01-05 21:27:36 +0530

    Update bans.py

--- mrjoker/modules/bans.py
+++ mrjoker/modules/bans.py
@@ -428,7 +428,7 @@ dispatcher.add_handler(UNBAN_HANDLER)
 dispatcher.add_handler(ROAR_HANDLER)
 dispatcher.add_handler(PUNCHME_HANDLER)
 
-__mod_name__ = "Muting"
+__mod_name__ = "ᴍᴜᴛɪɴɢ🤐"
 __handlers__ = [
     BAN_HANDLER,
     TEMPBAN_HANDLER,
