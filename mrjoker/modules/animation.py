a1dd8cb7c26e2dfe259a0226c0b2b4ea3fa036ab
Vickyftw <90129845+Vickyftw@users.noreply.github.com>
2022-01-08 23:59:54 +0530

    Update animation.py

--- mrjoker/modules/animation.py
+++ mrjoker/modules/animation.py
@@ -293,7 +293,7 @@ dispatcher.add_handler(MOONANIMATION_HANDLER)
 dispatcher.add_handler(CLOCKANIMATION_HANDLER)
 dispatcher.add_handler(BLOCKANIMATION_HANDLER)
 
-__mod_name__ = "Animation"
+__mod_name__ = "ᴀɴɪᴍᴀᴛɪᴏɴ🧸"
 __command_list__ = [
     "love",
     "hack",
