c1947cf084e1de6c434a327c4df8e38e9423d292
Vickyftw <90129845+Vickyftw@users.noreply.github.com>
2022-01-09 20:15:06 +0530

    Update warns.py

--- mrjoker/modules/warns.py
+++ mrjoker/modules/warns.py
@@ -515,7 +515,7 @@ be a sentence, encompass it with quotes, as such: `/addwarn "very angry" This is
 🔹 `/strongwarn` <on/yes/off/no>*:*
 """
 
-__mod_name__ = "Warns"
+__mod_name__ = "ᴡᴀʀɴꜱ❕"
 
 WARN_HANDLER = CommandHandler(["warn", "dwarn"], warn_user, filters=Filters.group)
 RESET_WARN_HANDLER = CommandHandler(
