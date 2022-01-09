b7cac1a0b67d527bb73d855a4ad2d0c83e002386
Vickyftw <90129845+Vickyftw@users.noreply.github.com>
2022-01-09 20:19:16 +0530

    Update ytdl.py

--- mrjoker/modules/ytdl.py
+++ mrjoker/modules/ytdl.py
@@ -134,5 +134,5 @@ __help__ = """
  ╔ - `/yt` <text>: perform a youtube search
  ╚ - `/ytaudio` <link> or /ytvideo <link>: Downlods a video or audio from a youtube video to the bots local server
 """
-__mod_name__ = "YouTube"        
+__mod_name__ = "ʏᴏᴜᴛᴜʙᴇ🎥"        
         
