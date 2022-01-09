f7c19c66a7bed8964eece700cda62933e2386b6e
Vickyftw <90129845+Vickyftw@users.noreply.github.com>
2022-01-09 19:53:32 +0530

    Update lock.py

--- mrjoker/modules/lock.py
+++ mrjoker/modules/lock.py
@@ -576,7 +576,7 @@ Locking bots will stop non-admins from adding bots to the chat.
  🔹 Unlocking permission *pin* will allow members (non-admins) to pinned a message in a group
 """
 
-__mod_name__ = "ʟᴏᴄᴋꜱ🔒"
+__mod_name__ = "ʟᴏᴄᴋꜱ🔐"
 
 LOCKTYPES_HANDLER = DisableAbleCommandHandler("locktypes", locktypes)
 LOCK_HANDLER = CommandHandler("lock", lock, pass_args=True)  # , filters=Filters.group)
