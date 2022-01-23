ed440b3681a423569040618050660595a5ff1469
Vickyftw <90129845+Vickyftw@users.noreply.github.com>
2022-01-23 18:08:00 +0530

    Update cust_filters.py

--- mrjoker/modules/cust_filters.py
+++ mrjoker/modules/cust_filters.py
@@ -607,11 +607,11 @@ def __chat_settings__(chat_id, user_id):
 
 
 __help__ = """
- 🔹 `/filters`*:* List all active filters saved in the chat.
+ 🔹 /filters : List all active filters saved in the chat.
 
-*Admin only:*
+ᴀᴅᴍɪɴꜱ ᴏɴʟʏ :
 
- 🔹 `/filter` <keyword> <reply message>*:* Add a filter to this chat. The bot will now reply that message whenever 'keyword'\
+ 🔹 /filter <keyword> <reply message>*:* Add a filter to this chat. The bot will now reply that message whenever 'keyword'\
 is mentioned. If you reply to a sticker with a keyword, the bot will reply with that sticker. NOTE: all filter \
 keywords are in lowercase. If you want your keyword to be a sentence, use quotes. eg: /filter "hey there" How you \
 doin?
@@ -625,11 +625,11 @@ doin?
  %%%
  Reply 3`
 
- 🔹 `/stop` <filter keyword>*:* Stop that filter.
+ 🔹 /stop <filter keyword> : Stop that filter.
 
-*Chat creator only:*
+Chat creator only :
 
- 🔹 `/removeallfilters`*:* Remove all chat filters at once.
+ 🔹  /removeallfilters : Remove all chat filters at once.
 
 *Note*: Filters also support markdown formatters like: {first}, {last} etc.. and buttons.
 Check `/markdownhelp` to know more!
