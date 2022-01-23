b9a7edb7e24d517146023786cc31195367b8ddf0
Vickyftw <90129845+Vickyftw@users.noreply.github.com>
2022-01-23 18:10:37 +0530

    Update chatbots.py

--- mrjoker/modules/chatbots.py
+++ mrjoker/modules/chatbots.py
@@ -1,14 +1,14 @@
 __help__= f"""
 💕 *Angel Luna AI Chatbot* 
 
- 🔹 `/chatbot` on/off: Enables and disables Luna AI Chat mode (EXCLUSIVE)
- 🔹 `/chatbot en `: Enables English only chatbot
+ 🔹  /chatbot on/off: Enables and disables Luna AI Chat mode (EXCLUSIVE)
+ 🔹  /chatbot en : Enables English only chatbot
  
 *Powered by* : [Luna ChatBot](https://github.com/TheHamkerCat/LunaChatBot)
 
 💕 *Angel Kuki AI Chatbot* 
 
-🔹 `/addkuki `: Enables and Disables Kuki AI Chat mode (EXCLUSIVE)
+🔹 /addkuki : Enables and Disables Kuki AI Chat mode (EXCLUSIVE)
 
 *Powered by* : [Kuki Chatbot](https://github.com/MoeZilla/KukiChatbot)
 """
