5af50b517b9cb54d798bc5be752a97be52a960b5
Vickyftw <90129845+Vickyftw@users.noreply.github.com>
2022-01-25 10:55:08 +0530

    Update purge.py

--- mrjoker/modules/purge.py
+++ mrjoker/modules/purge.py
@@ -96,11 +96,11 @@ async def delete_messages(event):
 
 
 __help__ = """
-*Admin only:*
+*ᴀᴅᴍɪɴꜱ ᴏɴʟʏ:*
 
- 🔹 `/del`*:* deletes the message you replied to.
- 🔹 `/purge`*:* deletes all messages between this and the replied to message.
- 🔹 `/purge` <integer X>*:* deletes the replied message, and X messages following it if replied to a message.
+ 🔹  /del :  deletes the message you replied to.
+ 🔹  /purge :  deletes all messages between this and the replied to message.
+ 🔹  /purge <integer X> : deletes the replied message, and X messages following it if replied to a message.
 """
 
 __mod_name__ = "ᴘᴜʀɢᴇꜱ⚡️"
