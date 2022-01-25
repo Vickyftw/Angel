059a26ff8310e7f02a9e5ddef65286bdc3ea0e79
Vickyftw <90129845+Vickyftw@users.noreply.github.com>
2022-01-25 10:36:18 +0530

    Update mjplayer.py

--- mrjoker/modules/mjplayer.py
+++ mrjoker/modules/mjplayer.py
@@ -2,11 +2,11 @@
 __mod_name__ = "ᴍᴇᴅɪᴀ🪄"
 
 __help__ = """
-🔹 `/video` <query>: download video from youtube
-🔹 `/fbdl` <query>: Facebook Video Download
-🔹 `/deezer` <query>: download from deezer
-🔹 `/saavn` <query>: download song from saavn.
-🔹 `/song` <queryi>: download song from yt servers. 
-🔹 `/lyrics` <song name<> : This plugin searches for song lyrics with song name.
-🔹 `/glyrics` <song namei> : This plugin searches for song lyrics with song name and artist.
+🔹  /video  <query>: download video from youtube
+🔹  /fbdl  <query>: Facebook Video Download
+🔹  /deezer  <query>: download from deezer
+🔹  /saavn  <query>: download song from saavn.
+🔹  /song  <queryi>: download song from yt servers. 
+🔹  /lyrics  <song name<> : This plugin searches for song lyrics with song name.
+🔹  /glyrics  <song name> : This plugin searches for song lyrics with song name and artist.
 """
