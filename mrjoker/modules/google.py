62c16eafd006e7d63caaa47eacd49353ebb19514
Vickyftw <90129845+Vickyftw@users.noreply.github.com>
2022-01-25 15:48:24 +0530

    Update google.py

--- mrjoker/modules/google.py
+++ mrjoker/modules/google.py
@@ -275,7 +275,7 @@ async def apk(e):
         await e.reply("Exception Occured:- " + str(err))
 
 
-__mod_name__ = "ꜱᴇᴀʀᴄʜ🔎"
+__mod_name__ = "Search"
 
 __help__ = """
  🔹  /google <text> : Perform a google search
