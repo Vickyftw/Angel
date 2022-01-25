1c1261cf17fc9c5a03e890bfdf6749b5b56a4712
Vickyftw <90129845+Vickyftw@users.noreply.github.com>
2022-01-25 15:45:32 +0530

    Update cust_filters.py

--- mrjoker/modules/cust_filters.py
+++ mrjoker/modules/cust_filters.py
@@ -636,7 +636,7 @@ Check `/markdownhelp` to know more!
 
 """
 
-__mod_name__ = "ꜰɪʟᴛᴇʀꜱ📕"
+__mod_name__ = "Filters"
 
 FILTER_HANDLER = CommandHandler("filter", filters)
 STOP_HANDLER = CommandHandler("stop", stop_filter)
