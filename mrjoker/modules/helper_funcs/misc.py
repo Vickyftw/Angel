34400b34c7fbb447cc625e5a6e410fa8ae953f2e
Vickyftw <90129845+Vickyftw@users.noreply.github.com>
2022-01-25 11:41:17 +0530

    Update misc.py

--- mrjoker/modules/helper_funcs/misc.py
+++ mrjoker/modules/helper_funcs/misc.py
@@ -66,12 +66,12 @@ def paginate_modules(page_n: int, module_dict: Dict, prefix, chat=None) -> List:
     # can only have a certain amount of buttons side by side
     if len(pairs) > 10:
         pairs = pairs[modulo_page * 10:10 * (modulo_page + 1)] + [
-            (EqInlineKeyboardButton("⬅️ 𝗕𝗔𝗖𝗞", callback_data="{}_prev({})".format(prefix, modulo_page)),
-                EqInlineKeyboardButton("𝗖𝗟𝗢𝗦𝗘 ❌", callback_data="mrjoker_back"),
-             EqInlineKeyboardButton("𝗡𝗘𝗫𝗧 ➡️", callback_data="{}_next({})".format(prefix, modulo_page)))]
+            (EqInlineKeyboardButton("🔺 ʙᴀᴄᴋ ", callback_data="{}_prev({})".format(prefix, modulo_page)),
+                EqInlineKeyboardButton("✖️ᴄʟ⭕️ꜱᴇ✖️", callback_data="mrjoker_back"),
+             EqInlineKeyboardButton("ɴᴇxᴛ 🔻", callback_data="{}_next({})".format(prefix, modulo_page)))]
 
     else:
-        pairs += [[EqInlineKeyboardButton("⬅️ 𝗕𝗔𝗖𝗞", callback_data="mrjoker_back")]]
+        pairs += [[EqInlineKeyboardButton("⬅️ ʙᴀᴄᴋ", callback_data="mrjoker_back")]]
 
     return pairs
 
