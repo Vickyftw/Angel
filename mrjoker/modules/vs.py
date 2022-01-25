69ade7a7ad6bafd6ad1f28fe21df4a26c9792932
Vickyftw <90129845+Vickyftw@users.noreply.github.com>
2022-01-25 13:30:45 +0530

    Update vs.py

--- mrjoker/modules/vs.py
+++ mrjoker/modules/vs.py
@@ -1,17 +1,28 @@
 __help__ = """
 *All Command List:*
-🔹 `/vplay` (reply to video or yt/live url) - to stream video
-🔹 `/vstop ` - stop the video streaming
-🔹 `/jvsong` (song name) - download song from YT
-🔹 `/vsong` (video name) - download video from YT
-🔹 `/lyric` (song name) - lyric scrapper
-🔹 `/vjoin` - invite assistant join to your group
-🔹 `/vleave` - order assistant leave from your group
+
+🔹  /vplay  (reply to video or yt/live url) - to stream video
+🔹  /vstop  - stop the video streaming
+🔹  /jvsong  (song name) - download song from YT
+🔹  /vsong  (video name) - download video from YT
+🔹  /lyric  (song name) - lyric scrapper
+🔹   /vjoin  - invite assistant join to your group
+🔹   /vleave  - order assistant leave from your group
+
+๏ Command for admins only.
+ • /pause - To pause the playback.
+ • /resume - To resuming the playback You've paused.
+ • /skip - To skipping the player.
+ • /end - For end the playback.
+ • /ping - Cheak bot Alive.
+
+๏ Command for all members.
+ • /play <query /reply audio> - Playing music via YouTube.
 
 *SUDO ONLY:*
-🔹 `/rmdl` - remove all downloaded files
-🔹 `/wipe` - remove all downloaded raw files
-🔹 `/leaveall` - order assistant leave from all group
+🔹  /rmdl  - remove all downloaded files
+🔹  /wipe  - remove all downloaded raw files
+🔹  /leaveall  - order assistant leave from all group
 
 *HOW TO USE THIS BOT:*
 1.) first, add me to your group.
@@ -20,7 +31,7 @@ __help__ = """
 4.) turn on the voice chat first before start to stream video.
 5.) type /vplay (reply to video) to start streaming.
 6.) type /vstop to end the video streaming.
-Assitant Id :@FaLLenx4ngeL
+7.) Assitant Id :@FaLLenx4ngeL
 
 💡 *Note: stream & stop command can only be executed by group admin only!*
 
