2147bc90b6922932e0de4962b26558f50a544597
Vickyftw <90129845+Vickyftw@users.noreply.github.com>
2022-01-25 11:09:19 +0530

    Update shippering.py

--- mrjoker/modules/shippering.py
+++ mrjoker/modules/shippering.py
@@ -77,6 +77,6 @@ __New couple of the day may be chosen at 12AM {tomorrow}__"""
 
 
 __help__ = """
- 🔹 `/couples` - To Choose Couple Of The Day
+ 🔹 /couples - To Choose Couple Of The Day
  """
 __mod_name__ = "ᴄᴏᴜᴘʟᴇꜱ👩‍❤️‍👨"
